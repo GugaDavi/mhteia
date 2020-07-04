5b7e505c04586a562d46580e531668fd19951efa
Fliper Gustavo <gguedes.tech@gmail.com>
2020-07-03 21:11:36 -0300

    create bottom navigation bar

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import BottomNavigatior from "./bottomNavigator";
+
+const Routes: React.FC = () => {
+  return <BottomNavigatior />;
+};
+
+export default Routes;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #ffc;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 21px;
+  font-weight: bold;
+`;
